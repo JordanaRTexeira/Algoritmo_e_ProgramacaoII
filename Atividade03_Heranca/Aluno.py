9bb7959029726e66eb6609c50bf2dcc18343d862
JordanaRTexeira <80719908+JordanaRTexeira@users.noreply.github.com>
2021-03-24 16:57:51 -0300

    Delete Atividade03_Heranca directory

--- Atividade03_Heranca/Aluno.py
+++
@@ -1,9 +0,0 @@
-class Aluno:
-    def __init__(self, codigo, nome, matricula):
-        self.codigo    = codigo
-        self.nome      = nome
-        self.matricula = matricula
-
-
-    def imprimirAluno(self):
-        print(f"--- DADOS ALUNO --- \n -Codigo Aluno(a): {self.codigo} \n -Nome Aluno(a): {self.nome} \n -Matricula: {self.matricula}")
